7ad723af6af3f681730aa7e6497d99640bd9df78
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-15 10:53:49 -0500

    render a variable

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+const myDiv = <div>I am some text</div>;
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+ReactDOM.render(myDiv, document.getElementById('container'));
 registerServiceWorker();
